47cf12233d688c1881002a79b1a5ae13c31ddba5
KyuRicard <ricardboscholivar@gmail.com>
2015-11-23 11:50:59 +0100

    Phidget Integration

--- Calendari/Calendari/Form1.Designer.cs
+++ Calendari/Calendari/Form1.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Setmana = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Setmana
@@ -41,11 +43,21 @@
             this.Setmana.TabIndex = 0;
             this.Setmana.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(211, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
             // Calendari
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(515, 509);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Setmana);
             this.Name = "Calendari";
             this.Text = "Prototip del calendari";
@@ -59,6 +71,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox Setmana;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Calendari/Calendari/Form1.cs
+++ Calendari/Calendari/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PhidgetInteraction;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using Timer = System.Threading.Timer;
 
 namespace Calendari
 {
@@ -15,15 +11,29 @@ namespace Calendari
         DateTime mc;
         long timePressed = 0;
         bool isPressed = false;
+        InterPhi phi;
+        Timer timer1;
+
+        delegate void SetTextCallback(string txt);
 
         public Calendari()
         {
             InitializeComponent();
             mc = DateTime.Now;
             GetSetmanaActual();
+
+            phi = new InterPhi();
+
+            phi.AddSensor(new Sensor("0", "A", 850));
+            phi.AddSensor(new Sensor("1", "D", 850));
+            phi.AddSensor(new Sensor("2", "SPACE", 500));
+
+            phi.StartPhidget();
+       
+            timer1 = new System.Threading.Timer(CheckPhidgets, null, 0, 250);
         }
 
-        private void Calendari_KeyDown(object sender, KeyEventArgs e)
+        private void Calendari_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
         {
             switch (e.KeyData)
             {
@@ -49,9 +59,26 @@ namespace Calendari
         {
             DateTime monday = mc.AddDays(((int)mc.DayOfWeek - 1));
             DateTime sunday = monday.AddDays(6);
-            Setmana.Text = string.Format("Setmana del {0} de {1} de {2} al {3} de {4} de {5}",
-                monday.Day, GetMonth(monday.Month), monday.Year, sunday.Day, GetMonth(sunday.Month), sunday.Year);
+            SetText(string.Format("Setmana del {0} de {1} de {2} al {3} de {4} de {5}",
+                monday.Day, GetMonth(monday.Month), monday.Year, sunday.Day, GetMonth(sunday.Month), sunday.Year), Setmana);
         }
+
+        private void SetText(string text, TextBox tb)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (tb.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                tb.Text = text;
+            }
+        }
+
 
         private string GetMonth(int month)
         {
@@ -87,7 +114,7 @@ namespace Calendari
             }
         }
 
-        private void Calendari_KeyUp(object sender, KeyEventArgs e)
+        private void Calendari_KeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
         {
             if (e.KeyData == Keys.Space)
             {
@@ -105,5 +132,31 @@ namespace Calendari
             }
         }
 
+        private void CheckPhidgets(object o)
+        {
+            List<Sensor> sensors = phi.GetKeys();
+
+            foreach (Sensor s in sensors)
+            {
+                Console.WriteLine("Sensor {0} Value {1}", s.Value, s.Activated);
+            }
+            
+            if (phi.GetValue("A"))
+            {
+                Calendari_KeyDown(this, new KeyEventArgs(Keys.A));
+            }
+            else if (phi.GetValue("D"))
+            {
+                Calendari_KeyDown(this, new KeyEventArgs(Keys.D));
+            }
+            else if (phi.GetValue("SPACE"))
+            {
+                Calendari_KeyDown(this, new KeyEventArgs(Keys.Space));
+            }
+            else if (!phi.GetValue("SPACE") && isPressed)
+            {
+                Calendari_KeyUp(this, new KeyEventArgs(Keys.Space));
+            }
+        }
     }
 }
